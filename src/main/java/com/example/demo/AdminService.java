355499ca92a55d0b4beae0406e763106bea820b8
Benjamin Billet <contact@benjaminbillet.fr>
2019-12-21 09:07:21 +0100

    Automated generation

---
+++ src/main/java/com/example/demo/AdminService.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import com.example.demo.api.AdminsApiDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminService implements AdminsApiDelegate {
+}
