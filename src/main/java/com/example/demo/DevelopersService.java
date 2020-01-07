7832220932c56452ccc4cdb7e9092a7f7d12b3b6
Benjamin Billet <contact@benjaminbillet.fr>
2020-01-07 08:58:41 +0100

    Added missing service

---
+++ src/main/java/com/example/demo/DevelopersService.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import com.example.demo.api.DevelopersApiDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevelopersService implements DevelopersApiDelegate {
+}
